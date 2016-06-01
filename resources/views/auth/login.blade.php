352491618bf0bdb141b6d50524f88d8ef0a902e1
Rudi Olckers <d3vimust@gmail.com>
2016-06-01 21:30:12 +1200

    Add selenium automation support to run on travis

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,23 +11,23 @@
 
             <div class="col-md-4">
 
-                {{ Form::open(['role' => 'form', 'url' => '/auth/login']) }}
+                {{ Form::open(['id' => 'login-form', 'role' => 'form', 'url' => '/auth/login']) }}
 
                     {!! csrf_field() !!}
 
                     <div class='form-group'>
-                        {{ Form::text('username', null, ['placeholder' => 'Email', 'class' => 'form-control']) }}
+                        {{ Form::text('username', null, ['id' => 'username', 'placeholder' => 'Email', 'class' => 'form-control']) }}
                     </div>
 
                     <div class='form-group'>
-                        {{ Form::password('password', ['placeholder' => 'Password', 'class' => 'form-control']) }}
+                        {{ Form::password('password', ['id' => 'password', 'placeholder' => 'Password', 'class' => 'form-control']) }}
                     </div>
 
                     <div class='form-group'>
                         @include('partials/errors')
                     </div>
 
-                    {{ Form::submit('Sign in', ['class' => 'btn btn-primary btn-small btn-block']) }}
+                    {{ Form::submit('Sign in', ['id' => 'submit-button', 'class' => 'btn btn-primary btn-small btn-block']) }}
 
                 {{ Form::close() }}
 
