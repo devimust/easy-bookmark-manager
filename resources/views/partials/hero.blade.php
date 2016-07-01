7d23461a9b81bec12a197d27ebd5af19b3c4c63e
Rudi Olckers <d3vimust@gmail.com>
2016-07-02 11:24:26 +1200

    Remove main section in hero when opening via chrome extension to give more realestate.

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -3,8 +3,8 @@
             <div class="col-md-12">
                 <div class="page-header">
                     <h1>
-                        <small class="hide-for-window"><a href class="bookmark-me-link glyphicon glyphicon-bookmark" role="button" aria-hidden="true"></a></small>
-                        <span class="hide-for-window">Bookmarks</span> <small>Easy Bookmark Manager</small>
+                        <small><a href class="bookmark-me-link glyphicon glyphicon-bookmark" role="button" aria-hidden="true"></a></small>
+                        <span>Bookmarks</span> <small>Easy Bookmark Manager</small>
                     </h1>
                 </div>
             </div>
