8e4713cf0b7d0b9c707c0f0edc3bb2fb7467f201
amiceli <miceli.antoine@gmail.com>
2016-11-20 20:59:23 +0100

    Add sha256 for user account validation

---
+++ database/migrations/2016_11_20_184423_add_user_validation_hash.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserValidationHash extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->string('hashValidation')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function ($table) {
+            $table->dropColumn('hashValidation');
+        });
+    }
+}
