d851a1f803e0a9db45d04a83c7f0ac7fd1aa5773
Rudi Olckers <rudi.olckers@gmail.com>
2016-11-26 21:31:17 +1300

    Add prep and create release candidate scripts.

---
+++ docker/create-rc.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Create release candidate..."
+echo
+
+if [ ! -d .git ]; then
+  echo "ensure this script is run from the project root"
+  exit 1
+fi
+
+docker-compose run --rm gulp --production
+
+find ./storage -type 'f' | grep -v ".gitignore" | xargs rm -f
+
+TIMESTAMP=$(date +%s)
+
+tar \
+    --owner=0 --group=0 \
+    -czf ./easy-bookmark-manager.$TIMESTAMP.tar.gz \
+    app/ \
+    bootstrap/ \
+    config/ \
+    database/ \
+    public/ \
+    resources/ \
+    storage/ \
+    vendor/ \
+    .env.example \
+    .htaccess\
+    server.php
